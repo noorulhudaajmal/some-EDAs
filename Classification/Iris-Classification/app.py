adb142ee5ee60416ed7d78f56a9421604e936276
noorulhudaajmal <68446582+noorulhudaajmal@users.noreply.github.com>
2022-08-12 16:46:47 +0500

    files added

---
+++ Classification/Iris-Classification/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("url_goes_here")
+    }
+   .sidebar .sidebar-content {
+        background: url("url_goes_here")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
